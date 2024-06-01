fe460089dd90766734e3b16ed7e350fe97d78792
QianjunL <qianjunliang.ql@gmail.com>
2024-05-31 23:57:58 -0600

    upadte center to add add icon

--- src/components/Center/Center.jsx
+++ src/components/Center/Center.jsx
@@ -1,7 +1,20 @@
+import { Avatar } from "@mui/material";
 import React from "react";
+import AddIcon from "@mui/icons-material/Add";
 
 const Center = () => {
-  return <div>Center</div>;
+  return (
+    <div className="px-20">
+      <div className="flex items-center p-5 rounded-b-md">
+        <div className="flex flex-col items-center mr-4 cursor-pointer">
+          <Avatar sx={{ width: "5rem", height: "5rem" }} className="">
+            <AddIcon sx={{ fontSize: "3rem" }} />
+          </Avatar>
+          <p>New</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Center;
